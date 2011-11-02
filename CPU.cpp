ddbd73cf70230a338571b07ff9c7d73e1abaa748
kaminagi <james.mitchell127@gmail.com>
2011-11-02 18:54:58 -0500

    Updating changes adding the control.

--- CPU.cpp
+++ CPU.cpp
@@ -72,9 +72,9 @@ struct ControlOut
 	bool memRead;
 	bool memToReg;
 	bool memWrite;
-	bool aluOP;
-	bool aluSRC;
 	bool regWrite;
+	bool aluSRC;
+	int aluOP[2];
 }
 
 int main()
@@ -113,13 +113,111 @@ void fetch()
 
 }
 
-ControlOut ControlUnit(int bit1, int bit2, int bit3)
+ControlOut ControlUnit(int bit1, int bit2, int bit3, int 4)
 {
-	ControlOut temp;
+	ControlOut temp = {false, false, false, false, false, false, false, {0,2}};
 	
 	//giant switch to set bits
+	//0000
+	//NOP
+	if(!bit1 && !bit2 && !bit3 && !bit4)
+	{
 
+	}
+	//0001
+	//add & sub
+	else if(!bit1 && !bit2 && !bit3 && bit4)
+	{
 
+	}
+	//0010
+	//lw
+	else if(!bit1 && !bit2 && bit3 && !bit4)
+	{
 
+	}
+	//0011
+	//sw
+	else if(!bit1 && !bit2 && bit3 && bit4)
+	{
+
+	}
+	//0100
+	//and, or, not, xor
+	else if(!bit1 && bit2 && !bit3 && !bit4)
+	{
+
+	}
+	//0101
+	//sll
+	else if(!bit1 && bit2 && !bit3 && bit4)
+	{
+
+	}
+	//0110
+	//srl
+	else if(!bit1 && bit2 && bit3 && !bit4)
+	{
+
+	}
+	//0111
+	//beq
+	else if(!bit1 && bit2 && bit3 && bit4)
+	{
+		temp.branch = true;
+	}
+	//1000
+	//blt
+	else if(bit1 && !bit2 && !bit3 && !bit4)
+	{
+		temp.branch = true;
+	}
+	//1001
+	//no function?
+	else if(bit1 && !bit2 && !bit3 && bit4)
+	{
+
+	}
+	//1010
+	//no function?
+	else if(bit1 && !bit2 && bit3 && !bit4)
+	{
+
+	}
+	//1011
+	//addi
+	else if(bit1 && !bit2 && bit3 && bit4)
+	{
+
+	}
+	//1100
+	//jump
+	else if(bit1 && bit2 && !bit3 && !bit4)
+	{
+
+	}
+	//1101
+	//no function?
+	else if(bit1 && bit2 && !bit3 && bit4)
+	{
+
+	}
+	//1110
+	//subi
+	else if(bit1 && bit2 && bit3 && !bit4)
+	{
+
+	}
+	//1111
+	//no function?
+	else if(bit1 && bit2 && bit3 && bit4)
+	{
+
+	}
+	else
+	{
+		//what did you do?!
+	}
+	
 	return temp;
 }
